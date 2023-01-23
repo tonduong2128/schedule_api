c92d9394a32f2620ae6d2fe43d55922437520690
duongtanton <20120598@student.hcmus.edu.vn>
2023-01-23 17:51:10 +0700

    add vehicle type

--- src/db/model/vehicle_type.js
+++ src/db/model/vehicle_type.js
@@ -5,7 +5,12 @@ import sequelize from "../index.js";
 
 const VehicleType = sequelize.define('vehicle_type', {
     name: DataTypes.STRING,
+    description: DataTypes.STRING,
 
+    teacherId: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+    },
     createdBy: {
         type: DataTypes.BIGINT,
         allowNull: false,

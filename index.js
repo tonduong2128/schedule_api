56117e35eacd2a1fa7b7a2269fcd041dbdcf0bca
duongtanton <20120598@student.hcmus.edu.vn>
2023-02-13 16:57:56 +0700

    fix cors

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ if (process.env.NODE_ENV == "production") {
 }
 
 const app = express()
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 app.use(cors({
   origin: '*',
   credentials: true,
